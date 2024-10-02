e85ef604c6e9b1d4bdb00f72da6ba1067a6c80b8
OlgaLesser <lesserolga@gmail.com>
2024-10-02 12:40:50 -0600

    Modules

---
+++ python_modules/0_calculation.py
@@ -0,0 +1,13 @@
+#!C:\Users\korch\AppData\Local\Programs\Python\Python312
+if __name__ == "__main__":
+    import calculation as calc
+    a = 4
+    b = 2
+    result_add = calc.add(a, b)
+    result_sub = calc.sub(a, b)
+    result_mul = calc.mul(a, b)
+    result_div = calc.div(a, b)
+    print("4 + 2 = ", result_add)
+    print("4 - 2 = ", result_sub)
+    print("4 * 2 = ", result_mul)
+    print("4 / 2 = ", result_div)
---
+++ python_modules/1_import_that.py
@@ -0,0 +1,3 @@
+#!C:\Users\korch\AppData\Local\Programs\Python\Python312
+from calculation import some_var
+print(some_var)
