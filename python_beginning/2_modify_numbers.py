b9f6341b687590fc27fa0767c065bbfe835b7509
OlgaLesser <lesserolga@gmail.com>
2025-01-13 10:29:11 -0700

    Changes

--- python_beginning/2_modify_numbers.py
+++ python_beginning/2_modify_numbers.py
@@ -1,6 +1,5 @@
 #!C:\Users\korch\AppData\Local\Programs\Python\Python312
 x = 3.141592653589793238
-y = round(x, 2)
-print(f"Learning Python is fun\"\' - {y}%")
-print("Learning Python is fun\"\' - {}%".format(y))
-print("Learning Python is fun\"\' - %.2f%%" % y)
+print(f"Learning Python is fun\"\' - {x:.2f}%")
+print("Learning Python is fun\"\' - {:.2f}%".format(x))
+print("Learning Python is fun\"\' - %.2f%%" % x)
