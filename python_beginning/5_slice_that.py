21e5c314ca627332d4c4f1be740b02a1af7f65c5
OlgaLesser <lesserolga@gmail.com>
2025-01-14 09:40:12 -0700

    Updates

--- python_beginning/5_slice_that.py
+++ python_beginning/5_slice_that.py
@@ -1,6 +1,3 @@
 #!C:\Users\korch\AppData\Local\Programs\Python\Python312
 string = "Webdriver Awesome Camp"
-first_word = string[0:9]
-second_word = string[10:17]
-third_word = string[18:22]
-print(first_word, second_word, third_word, sep='\n')
+print(string[0:9], string[10:17], string[18:22], sep='\n')
--- python_beginning/6_what_needed.py
+++ python_beginning/6_what_needed.py
@@ -1,4 +1,3 @@
 #!C:\Users\korch\AppData\Local\Programs\Python\Python312
-x = "There should be one-- and preferably only one --obvious way to do it. lthough that way may not be obvious at first unless you're Dutch."
-x = x[26:45] + x[82:86] + x[114:121]
-print(x)
+string = "There should be one-- and preferably only one --obvious way to do it. I though that way may not be obvious at first unless you're Dutch."
+print(string[26:46] + string[56:60] + string[116:123])
