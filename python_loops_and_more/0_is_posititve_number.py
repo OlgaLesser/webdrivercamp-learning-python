aed4cc454db4988e1521fa12198fd7e64b4c1aa9
OlgaLesser <lesserolga@gmail.com>
2025-01-14 09:53:28 -0700

    Update 0_is_posititve_number.py

--- python_loops_and_more/0_is_posititve_number.py
+++ python_loops_and_more/0_is_posititve_number.py
@@ -1,9 +1,9 @@
 #!C:\Users\korch\AppData\Local\Programs\Python\Python312
 import random
 random_num = random.randint(-10, 10)
-if random_num < 0:
-    print(str(random_num) + " is negative")
-elif random_num > 0:
-    print(str(random_num) + " is positive")
+if random_num > 0:
+    print(random_num, "is positive")
+elif random_num < 0:
+    print(random_num, "is negative")
 else:
-    print(str(random_num) + " is zero")
+    print(random_num, "is zero")
