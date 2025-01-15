8d94c91ccb0b694c9bc27cd3bf57320b517cc316
OlgaLesser <lesserolga@gmail.com>
2025-01-15 09:11:16 -0700

    Update 0_calculation.py

--- python_modules/0_calculation.py
+++ python_modules/0_calculation.py
@@ -1,13 +1,9 @@
 #!C:\Users\korch\AppData\Local\Programs\Python\Python312
 if __name__ == "__main__":
-    import calculation as calc
+    from calculation import add, sub, mul, div
     a = 4
     b = 2
-    result_add = calc.add(a, b)
-    result_sub = calc.sub(a, b)
-    result_mul = calc.mul(a, b)
-    result_div = calc.div(a, b)
-    print("4 + 2 = ", result_add)
-    print("4 - 2 = ", result_sub)
-    print("4 * 2 = ", result_mul)
-    print("4 / 2 = ", result_div)
+    print(a, "+", b, "=", add(a, b))
+    print(a, "-", b, "=", sub(a, b))
+    print(a, "*", b, "=", mul(a, b))
+    print(a, "/", b, "=", div(a, b))
