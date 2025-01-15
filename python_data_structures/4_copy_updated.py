b5f5dc183a34fb5101a193f85597c19590f58dfc
OlgaLesser <lesserolga@gmail.com>
2025-01-15 10:16:41 -0700

    Update 4_copy_updated.py

--- python_data_structures/4_copy_updated.py
+++ python_data_structures/4_copy_updated.py
@@ -1,17 +1,17 @@
 #!C:\Users\korch\AppData\Local\Programs\Python\Python312
 list_ = [5, 4, 3, 2, 1]
-list2 = list_.copy()
 index = 1
 element_to_replace = 5
 
 
-def replace_element(lst, ind, new_value):
-    if 0 < ind < len(lst):
-        lst[ind] = new_value
-        print('Copy:    ', lst)
+def replace_element(some_list, ind, new_value):
+    if ind < 0 or ind > len(some_list):
+        print(some_list)
     else:
-        print('Copy:    ', lst)
+        list_copy = some_list.copy()
+        list_copy[ind] = new_value
+        print("Copy:        ", list_copy)
+        print("Original:    ", some_list)
 
 
 replace_element(list_, index, element_to_replace)
-print('Original:', list2)
