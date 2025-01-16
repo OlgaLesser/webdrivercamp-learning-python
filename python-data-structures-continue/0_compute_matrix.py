727e982999efb8574943103884df87556489fbf7
OlgaLesser <lesserolga@gmail.com>
2025-01-16 09:43:57 -0700

    Update 0_compute_matrix.py

--- python-data-structures-continue/0_compute_matrix.py
+++ python-data-structures-continue/0_compute_matrix.py
@@ -1,17 +1,14 @@
 #!/C:\Users\korch\AppData\Local\Programs\Python\Python312
-def compute_matrix(matrix=None):
-    if matrix is None:
-        matrix = []
-    new_matrix = [list(map(lambda x: x**2, row)) for row in matrix]
-    return new_matrix
+def compute_matrix(some_matrix):
+    squared_matrix = [list(map(lambda x: x**2, row)) for row in some_matrix]
+    return squared_matrix
 
 
 if __name__ == "__main__":
-    my_matrix = [
-            [9, 8, 7],
-            [6, 5, 4],
-            [3, 2, 1]
-        ]
-    modified_matrix = compute_matrix(my_matrix)
-    print(f"Original: {my_matrix}")
-    print(f"Modified: {modified_matrix}")
+    matrix = [
+        [9, 8, 7],
+        [6, 5, 4],
+        [3, 2, 1]
+    ]
+    print(f"Original: {matrix}")
+    print(f"Modified: {compute_matrix(matrix)}")
