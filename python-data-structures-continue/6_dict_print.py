749b73e28fbb7c8dd09d79221e39d4e0f2bd2903
OlgaLesser <lesserolga@gmail.com>
2025-01-17 08:45:15 -0700

    Update 6_dict_print.py

--- python-data-structures-continue/6_dict_print.py
+++ python-data-structures-continue/6_dict_print.py
@@ -1,10 +1,9 @@
 #!/C:\Users\korch\AppData\Local\Programs\Python\Python312
-def dict_print(dict_):
-    my_list = sorted(list(dict_.keys()))
-    for i in my_list:
-        print(i, ": ", dict_[i])
+def dict_print(some_dict):
+    for key in sorted(some_dict):
+        print(f"{key}: {some_dict[key]}")
 
 
 if __name__ == "__main__":
-    my_dict = {"libs": (1, 2, 3), "x": "Selenium", "lang": ["Java", "Python"], "frame": "Behave", "set": set()}
-    dict_print(my_dict)
+    dict_ = {"libs": (1, 2, 3), "x": "Selenium", "lang": ["Java", "Python"], "frame": "Behave", "set": set()}
+    dict_print(dict_)
